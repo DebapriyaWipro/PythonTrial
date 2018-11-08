7604c543591870d656fd91f6c3790696ef524bd7
DebapriyaWipro <44835298+DebapriyaWipro@users.noreply.github.com>
2018-11-08 11:47:53 +0530

    Update First.py

--- First.py
+++ First.py
@@ -1,15 +1,18 @@
-import web
+#import web
+#
+#urls = (
+#    '/(.*)', 'hello'
+#)
+#app = web.application(urls, globals())
+#
+#class hello:        
+#    def GET(self, name):
+#        if not name: 
+#            name = 'world'
+#        return 'Hello, ' + name + '!'
+#
+#if __name__ == "__main__":
+#    app.run()
+#
 
-urls = (
-    '/(.*)', 'hello'
-)
-app = web.application(urls, globals())
-
-class hello:        
-    def GET(self, name):
-        if not name: 
-            name = 'world'
-        return 'Hello, ' + name + '!'
-
-if __name__ == "__main__":
-    app.run()
+print ("Hello Debapriyo")
